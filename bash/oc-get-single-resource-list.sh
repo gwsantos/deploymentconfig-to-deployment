e3fbb8504b7ead17ac90708715b6f8c496c4bcf0
Gustavo De Souza Santos <gdesouza@redhat.com>
2024-07-29 11:45:47 -0400

    updated gitignore and shell script

--- bash/oc-get-single-resource-list.sh
+++ bash/oc-get-single-resource-list.sh
@@ -2,9 +2,8 @@
 
 #Returns list of all kubernetes resources of a given kind in text format from the current openshift namespace.
 #User must be logged in and authenticated to an openshift cluster and be at the desired namespace/project to execute the script.
+#Resource kind should be passed IN PLURAL as the first argument - i.e. "routes", "deployments", "services" etc
 
 KIND=$(echo "$1" | awk '{print tolower($0)}')
-KIND_PLURAL="$KIND"s
 
-
-oc get "$KIND_PLURAL" --no-headersoc get "$KIND" -o template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' > ../temp-files/"$KIND"_list
+oc get "$KIND" --no-headers | oc get "$1" -o template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' > ../temp-files/"$KIND"_list
